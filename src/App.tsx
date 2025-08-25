da2abae8af8e2b029d674c004661541586b5e299
John Patrick J. Del Prado <patrick81304@gmail.com>
2025-08-25 17:11:41 +0800

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,3 @@ function App() {
 }
 
 export default App;
-
-//comment lang try sa pull
-
-//second comment try lang
-
-//third comment try role
-
-//4th comment merge try
-
-//5th comment last try
