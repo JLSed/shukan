ba70f8cfa39f6aca2afc97e60fcc83daf8f923a1
Jseebidi <ong.375007@novaliches.sti.edu.ph>
2025-08-25 17:17:04 +0800

    tinangal na po

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-// awesome
-// yo gurt
-// gurt: yo
 
-// add me on fb
-// hahaha
-// test
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
