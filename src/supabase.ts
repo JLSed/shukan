613f079a6997b4bc93847f86ba21897a9b287c88
Jseebidi <ong.375007@novaliches.sti.edu.ph>
2025-08-25 16:32:56 +0800

    added another comment

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 // wow
-
+// xd 
 
 
 export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
