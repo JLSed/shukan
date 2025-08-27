c585567797495bddca384f77786fe4a18a4e3e78
JLSed <seddd001@gmail.com>
2025-08-27 08:49:30 +0800

    fixed quest form, small changes

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,35 +1,13 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import QuestBoard from "../components/QuestList";
 import type { QuestListType } from "../types/card";
 import { supabase } from "../supabase";
+import QuestForm from "../components/QuestForm";
 
-const defaultNewTask: QuestListType = {
-  title: "",
-  description: "",
-  checklist: [],
-}
 
 const Homepage = () => {
-  const [newTask, setNewTask] = useState<QuestListType>(defaultNewTask)
   const [tasks, setTasks] = useState<QuestListType[]>([])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTask((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }))
-    console.log(newTask)
-  }
-
-  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const { error } = await supabase.from("tasks").insert(newTask).single()
-    if (error) {
-      alert(error.message)
-      return
-    }
-    setNewTask(defaultNewTask)
-  }
 
   const fetchTasks = async () => {
 
@@ -48,14 +26,8 @@ const Homepage = () => {
 
 
   return (
-    <div className="flex content-center justify-center p-lg">
-      <div>
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <input name="title" type="text" placeholder="Title" onChange={handleChange} />
-          <input name="description" type="text" placeholder="Description" onChange={handleChange} />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+    <div className="flex justify-center gap-4 p-lg">
+      <QuestForm />
       <QuestBoard questHeader="Daily Quest" questList={tasks} />
     </div>
   );
